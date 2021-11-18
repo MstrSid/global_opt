7780910393b22b315be685a4ab2d9ee4ea563f8c
Eugene Kos <mstrsid@live.com>
2021-11-18 16:42:12 +0300

    map section done

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,11 +7,36 @@ $(document).ready(function () {
 		autoWidth: true,
 		dots: false,
 		nav: true,
-		navText: ["<img src='../img/reviews/arrow_left.png'>","<img src='../img/reviews/arrow_right.png'>"],
+		navText: ["<img src='../img/reviews/arrow_left.png'>", "<img src='../img/reviews/arrow_right.png'>"],
 		responsive: {
 			600: {
 				items: 3
 			}
 		}
 	});
+});
+
+
+DG.then(function () {
+	let map,
+		myIcon;
+
+	map = DG.map('map', {
+		center: [55.74804966418222, 37.62726970531636],
+		zoom: 45,
+		scrollWheelZoom: false
+	});
+	myIcon = DG.icon({
+		iconUrl: '../icons/bullit.png',
+		iconSize: [48, 48]
+	});
+	DG.marker([55.74804966418222, 37.62726970531636], {
+		icon: myIcon
+	}).addTo(map).bindLabel('<span>г. Москва</span>' +
+		'ул. Садовническая, дом 5, офис 4-6<br>' +
+		'700 от м. Новокузнецкая<br>' +
+		'<a href="tel:+79264230100">Тел: +7 (926) 423 01 00</a><br>' +
+		'<a href="mailto:info@glopt.ru">info@glopt.ru</a>', {
+			static: true
+		});
 });
